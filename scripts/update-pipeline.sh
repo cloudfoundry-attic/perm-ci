1f32e80edfb101431419c49d4480bb10e18be101
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-08-30 12:25:04 -0700

    Fix update-pipeline.sh and git URLs

--- scripts/update-pipeline.sh
+++ scripts/update-pipeline.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 CONCOURSE_TARGET=perm
 CREDENTIALS_NOTE_NAME="Perm Pipeline Credentials"
 CRYPTDO_PASSWORD_NAME="Perm CI Cryptdo Password"
+
+function check_args() {
+  if [[ -z "${1:-}" ]]; then
+    echo "Must be called with \`update-pipeline.sh <pipeline>\`"
+    exit 1
+  fi
+}
 
 function check_installed() {
   local command
@@ -19,17 +26,20 @@ function check_installed() {
 function update_pipeline() {
   local pipeline
   local config_file
+  local cryptdo_password
   pipeline="$1"
   config_file="$(dirname -- "$0")/../pipelines/${pipeline}.yml"
+  cryptdo_password="$(lpass show "$CRYPTDO_PASSWORD_NAME" --password)"
 
   fly -t "$CONCOURSE_TARGET" set-pipeline \
     -p "$pipeline" \
     -c "$config_file" \
     -l <(lpass show "$CREDENTIALS_NOTE_NAME" --notes) \
-    -v cryptdo_password=<(lpass show "$CRYPTDO_PASSWORD_NAME" --password)
+    -v cryptdo_password="$cryptdo_password"
 }
 
 function main() {
+  check_args "$@"
   check_installed lpass
   check_installed fly
   # lpass sync
