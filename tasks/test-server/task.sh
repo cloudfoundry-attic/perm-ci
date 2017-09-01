69b4df844fc9762bbef6de7bc33ce0881556a201
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-09-01 10:06:13 -0700

    Use sh instead of bash for test-server task
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- tasks/test-server/task.sh
+++ tasks/test-server/task.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eu
 
 export GOPATH="${PWD}/gopath"
 export PATH="${GOPATH}/bin:${PATH}"
 
-pushd "${GOPATH}/src/code.cloudfoundry.org/perm"
-  go install code.cloudfoundry.org/perm/vendor/github.com/onsi/ginkgo/ginkgo
+cd "${GOPATH}/src/code.cloudfoundry.org/perm"
+go install code.cloudfoundry.org/perm/vendor/github.com/onsi/ginkgo/ginkgo
 
-  ginkgo -r -p -race -randomizeSuites -randomizeAllSpecs
-popd
+ginkgo -r -p -race -randomizeSuites -randomizeAllSpecs
