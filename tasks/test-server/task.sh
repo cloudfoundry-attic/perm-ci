41511ceb3c2fe08a09ac158808e63254ac87809e
Chris Hendrix <chendrix@pivotal.io>
2017-09-01 10:04:40 -0700

    fix test-server
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- tasks/test-server/task.sh
+++ tasks/test-server/task.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-export GOPATH="${PWD}/perm-release"
+export GOPATH="${PWD}/gopath"
 export PATH="${GOPATH}/bin:${PATH}"
 
-pushd "${GOPATH}/src/perm"
+pushd "${GOPATH}/src/code.cloudfoundry.org/perm"
   go install code.cloudfoundry.org/perm/vendor/github.com/onsi/ginkgo/ginkgo
 
   ginkgo -r -p -race -randomizeSuites -randomizeAllSpecs
