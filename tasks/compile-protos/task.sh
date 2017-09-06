3e619737c8ef7aafec2557ef48029ca24e590fac
Chris Hendrix <chendrix@pivotal.io>
2017-09-06 11:30:43 -0700

    Fix paths in compile-protos script
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- tasks/compile-protos/task.sh
+++ tasks/compile-protos/task.sh
@@ -2,15 +2,19 @@
 
 set -eux
 
+PERM_PACKAGE="code.cloudfoundry.org/perm"
+# shellcheck disable=SC2153
+PERM_PATH="${PERM_GOPATH}/src/${PERM_PACKAGE}"
+
 GOPATH="$PERM_GOPATH"
-go install "code.cloudfoundry.org/perm/vendor/github.com/gogo/protobuf/protoc-gen-gofast"
+go install "${PERM_PACKAGE}/vendor/github.com/gogo/protobuf/protoc-gen-gofast"
 
 PATH="${GOPATH}/bin:${PATH}"
 
 protoc \
-  --gofast_out=plugins=grpc:"${GOPATH}/bin" \
+  --gofast_out=plugins=grpc:"${PERM_PATH}/protos" \
   --ruby_out="${PERM_RB_PATH}/lib/perm/protos" \
   --plugin=protoc-gen-grpc="$(which grpc_tools_ruby_protoc_plugin)" \
   --grpc_out="${PERM_RB_PATH}/lib/perm/protos" \
-  -I="${PERM_GOPATH}/src/code.cloudfoundry.org/perm:${PERM_GOPATH}/src/code.cloudfoundry.org/perm/vendor" \
-  "${PERM_GOPATH}/src/code.cloudfoundry.org/perm/protos/"*.proto
+  -I="${PERM_PATH}/protos:${PERM_PATH}/vendor" \
+  "${PERM_PATH}/protos/"*.proto
