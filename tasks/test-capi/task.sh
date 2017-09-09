31408204e039e6d76f15c63003cb87484f1fde2a
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-09-08 17:39:12 -0700

    Add test-capi task
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

---
+++ tasks/test-capi/task.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PERM_HOSTNAME=localhost
+PERM_PORT=6283
+VERSION="$(cat version/version)"
+
+export PERM_RPC_HOST="${PERM_HOSTNAME}:${PERM_PORT}"
+
+tar -zxf "./perm-bin-dir/perm-${VERSION}.tgz" -C perm-bin-dir
+
+./perm-bin-dir/perm --listen-hostname "$PERM_HOSTNAME" --listen-port "$PERM_PORT" &
+CHILD_PID=$!
+
+function salt_earth() {
+  kill -9 "$CHILD_PID"
+}
+
+trap salt_earth EXIT
+
+CF_RUN_PERM_SPECS=true ./capi-ci/ci/test-unit/run_cc_unit_tests
