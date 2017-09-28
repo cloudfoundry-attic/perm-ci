e78e002e4fe8f35dfe119cdf05e5e36d4e4e10c9
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-09-28 13:37:00 -0700

    Do not explicitly start perm to test ruby client

--- tasks/test-ruby-client/task.sh
+++ tasks/test-ruby-client/task.sh
@@ -2,22 +2,9 @@
 
 set -eu
 
-PERM_HOSTNAME=localhost
-PERM_PORT=8888
 VERSION="$(cat version/version)"
 
-export PERM_RPC_HOST="${PERM_HOSTNAME}:${PERM_PORT}"
-
-tar -zxf "${PWD}/perm-bin-dir/perm-${VERSION}.tgz"
-
-./perm --listen-hostname "$PERM_HOSTNAME" --listen-port "$PERM_PORT" &
-CHILD_PID=$!
-
-function salt_earth() {
-  kill -9 "$CHILD_PID"
-}
-
-trap salt_earth EXIT
+tar -zxf "${PWD}/perm-bin-dir/perm-${VERSION}.tgz" -C /usr/local/bin
 
 pushd perm-rb
   bundle install
