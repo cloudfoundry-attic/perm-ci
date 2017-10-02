0b839959e9b6e9a804a241cd0fe645d7c3090937
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-10-02 14:57:34 -0700

    Make capi release from develop/master, not branches
    [#151615322]

--- tasks/create-capi-release/task.sh
+++ tasks/create-capi-release/task.sh
@@ -3,8 +3,6 @@
 set -eu
 
 TASKS_DIR="${PWD}/perm-ci"
-BRANCH="$(cat branch-dir/branch)"
-BRANCH_NAME="$(echo "${BRANCH}" | cut -d / -f 2)"
 TIMESTAMP="$(date +%s)"
 
 set +u
@@ -14,7 +12,7 @@ chruby 2.4.2
 set -u
 
 export SYNC_BLOBS="true"
-export TARBALL_NAME="${RELEASE_NAME}-release-${TIMESTAMP}-${BRANCH_NAME}-for-perm"
+export TARBALL_NAME="${RELEASE_NAME}-release-${TIMESTAMP}-for-perm"
 
 mv release-dir/parent-repo release
 "${TASKS_DIR}/tasks/create-release/task.sh"
