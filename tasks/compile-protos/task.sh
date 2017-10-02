a15b02ad026b00c528e38755de5f6105d9913a0d
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-10-02 10:19:36 -0700

    Verify ruby plugin exists before compiling protos

--- tasks/compile-protos/task.sh
+++ tasks/compile-protos/task.sh
@@ -11,10 +11,13 @@ go install "${PERM_PACKAGE}/vendor/github.com/gogo/protobuf/protoc-gen-gofast"
 
 PATH="${GOPATH}/bin:${PATH}"
 
+RUBY_PROTOC_PLUGIN="$(which grpc_tools_ruby_protoc_plugin)"
+: "${RUBY_PROTOC_PLUGIN:?"Did not find grpc_tools_ruby_protoc_plugin"}"
+
 protoc \
   --gofast_out=plugins=grpc:"${PERM_PATH}/protos" \
   --ruby_out="${PERM_RB_PATH}/lib/perm/protos" \
-  --plugin=protoc-gen-grpc="$(which grpc_tools_ruby_protoc_plugin)" \
+  --plugin=protoc-gen-grpc="$RUBY_PROTOC_PLUGIN" \
   --grpc_out="${PERM_RB_PATH}/lib/perm/protos" \
   -I="${PERM_PATH}/protos:${PERM_PATH}/vendor" \
   "${PERM_PATH}/protos/"*.proto
