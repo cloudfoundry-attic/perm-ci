28475b570f3668f4f7f6875c5c892e825789a5c4
Chris Hendrix <chendrix@pivotal.io>
2017-10-04 18:10:20 -0700

    fix cert path

--- tasks/create-perm-image-workspace/task.sh
+++ tasks/create-perm-image-workspace/task.sh
@@ -4,4 +4,4 @@ set -eu
 
 tar -zxf ./perm-bin-dir/perm-*.tgz -C perm-image-workspace
 cp perm-image-src/images/perm/Dockerfile perm-image-workspace
-cp perm-image-src/images/perm/certs/* perm-image-workspace
+cp perm-image-src/images/perm/certs/* perm-image-workspace/certs/
