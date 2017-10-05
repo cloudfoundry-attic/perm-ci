e288a68ea8b77c0ea4bd662a8a96119e1b10e5d0
Chris Hendrix <chendrix@pivotal.io>
2017-10-04 18:13:29 -0700

    Fix more

--- tasks/create-perm-image-workspace/task.sh
+++ tasks/create-perm-image-workspace/task.sh
@@ -4,4 +4,6 @@ set -eu
 
 tar -zxf ./perm-bin-dir/perm-*.tgz -C perm-image-workspace
 cp perm-image-src/images/perm/Dockerfile perm-image-workspace
+
+mkdir -p perm-image-workspace/certs
 cp perm-image-src/images/perm/certs/* perm-image-workspace/certs/
