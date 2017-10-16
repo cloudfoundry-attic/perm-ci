50d7c75e2ce7800c19c45d355194698e57fc6d64
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-10-16 12:12:08 -0700

    Start mysql before running ruby client tests
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- tasks/test-ruby-client/task.sh
+++ tasks/test-ruby-client/task.sh
@@ -6,6 +6,12 @@ VERSION="$(cat version/version)"
 
 tar -zxf "${PWD}/perm-bin-dir/perm-${VERSION}.tgz" -C /usr/local/bin
 
+# From capi-ci:
+# HACK: change access time on mysql files to copy them into the writable layer
+# Context: https://github.com/moby/moby/issues/34390
+find /var/lib/mysql/mysql -exec touch -c -a {} +
+service mysql restart
+
 pushd perm-rb
   bundle install
   bundle exec rake
