c3dbb0e31afdb40e1bd500450c79e3e005f979d0
Chris Hendrix <chendrix@pivotal.io>
2017-10-16 10:19:26 -0700

    Always start MySQL in CAPI tests
    [#150979010]
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- tasks/test-capi/task.sh
+++ tasks/test-capi/task.sh
@@ -6,4 +6,13 @@ VERSION="$(cat version/version)"
 
 tar -zxf "./perm-bin-dir/perm-${VERSION}.tgz" -C /usr/local/bin
 
+# Ensure that MySQL is definitely running since we currently only support MySQL,
+# and CAPI only starts MySQL if that's what the test run is using.
+#
+# From capi-ci:
+# HACK: change access time on mysql files to copy them into the writable layer
+# Context: https://github.com/moby/moby/issues/34390
+find /var/lib/mysql/mysql -exec touch -c -a {} +
+service mysql restart
+
 CF_RUN_PERM_SPECS=true ./capi-ci/ci/test-unit/run_cc_unit_tests
