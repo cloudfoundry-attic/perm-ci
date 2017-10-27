ac1ff08a0819e5d215b088494bc87d36980e0e43
Chris Hendrix <chendrix@pivotal.io>
2017-10-27 12:31:38 -0700

    Manually start mysql for ruby-based unit tests
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- tasks/test-ruby-client/task.sh
+++ tasks/test-ruby-client/task.sh
@@ -10,7 +10,12 @@ tar -zxf "${PWD}/perm-bin-dir/perm-${VERSION}.tgz" -C /usr/local/bin
 # HACK: change access time on mysql files to copy them into the writable layer
 # Context: https://github.com/moby/moby/issues/34390
 find /var/lib/mysql/mysql -exec touch -c -a {} +
-service mysql restart
+
+# We don't need to care about authentication here,
+# and for some reason the driver has trouble authenticating if we require authentication.
+# Ergo, no authentication in unit tests!
+mysqld_safe --skip-grant-tables > /dev/null &
+trap "killall -u mysql" EXIT
 
 pushd perm-rb
   bundle install
