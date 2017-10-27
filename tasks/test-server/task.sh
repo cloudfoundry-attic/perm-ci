b9e6eee954b35348c21987e1c982df3d913c4e6d
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-10-27 10:52:24 -0700

    Run server unit tests with unauthenticated mysql
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- tasks/test-server/task.sh
+++ tasks/test-server/task.sh
@@ -5,7 +5,11 @@ set -eu
 export GOPATH="${PWD}/gopath"
 export PATH="${GOPATH}/bin:${PATH}"
 
-service mysql restart
+# We don't need to care about authentication here,
+# and for some reason the driver has trouble authenticating if we require authentication.
+# Ergo, no authentication in unit tests!
+mysqld_safe --skip-grant-tables > /dev/null &
+trap "killall -u mysql" EXIT
 
 cd "${GOPATH}/src/code.cloudfoundry.org/perm"
 go install code.cloudfoundry.org/perm/vendor/github.com/onsi/ginkgo/ginkgo
