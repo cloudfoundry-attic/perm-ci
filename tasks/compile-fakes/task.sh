b4c4096acd0395e7a1f2db7262e5472de15ded7d
Chris Hendrix <chendrix@pivotal.io>
2017-11-08 14:17:38 -0800

    Able to run compile-fakes against other paths

--- tasks/compile-fakes/task.sh
+++ tasks/compile-fakes/task.sh
@@ -2,19 +2,19 @@
 
 set -eu
 
-PERM_PACKAGE="code.cloudfoundry.org/perm"
+PACKAGE="${PACKAGE:-code.cloudfoundry.org/perm}"
 # shellcheck disable=SC2153
-PERM_PATH="${PERM_GOPATH}/src/${PERM_PACKAGE}"
+PACKAGE_PATH="${PERM_GOPATH}/src/${PACKAGE}"
 
 GOPATH="$PERM_GOPATH"
 
 # Workaround because of
 # https://github.com/maxbrunsfeld/counterfeiter/issues/75
 # Add another GOPATH that's secretly the perm vendor directory
-ln -s "${PERM_PATH}/vendor" "${PERM_PATH}/src"
+ln -s "${PACKAGE_PATH}/vendor" "${PACKAGE_PATH}/src"
 
-pushd "${PERM_PATH}" > /dev/null
-  GOPATH=$GOPATH:$PERM_PATH go generate ./...
+pushd "${PACKAGE_PATH}" > /dev/null
+  GOPATH=$GOPATH:$PACKAGE_PATH go generate ./...
 popd > /dev/null
 
-unlink "${PERM_PATH}/src"
+unlink "${PACKAGE_PATH}/src"
