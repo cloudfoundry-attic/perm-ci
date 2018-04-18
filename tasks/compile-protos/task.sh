54469f11ca3995f3ce8a79e742d5a22af6900ba3
Slawek Ligus <sligus@pivotal.io>
2018-04-18 10:51:34 -0700

    Ensure the protos are built from the new location
    [#156768438]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- tasks/compile-protos/task.sh
+++ tasks/compile-protos/task.sh
@@ -3,20 +3,20 @@
 set -eu
 
 WORKSPACE="${HOME}/workspace"
-PERM_GO_PACKAGE="code.cloudfoundry.org/perm-go"
+PERM_PACKAGE="code.cloudfoundry.org/perm"
 # shellcheck disable=SC2153
-PERM_GO_PATH="${GOPATH}/src/${PERM_GO_PACKAGE}"
-PROTOS_PATH="${WORKSPACE}/perm-release/src/code.cloudfoundry.org/perm/protos"
+PERM_PACKAGE_PATH="${GOPATH}/src/${PERM_PACKAGE}"
+PROTOS_PATH="${PERM_PACKAGE_PATH}/protos"
 
-go install "${PERM_GO_PACKAGE}/vendor/github.com/gogo/protobuf/protoc-gen-gofast"
+go install "${PERM_PACKAGE}/vendor/github.com/gogo/protobuf/protoc-gen-gofast"
 
 RUBY_PROTOC_PLUGIN="$(which grpc_tools_ruby_protoc_plugin)"
 : "${RUBY_PROTOC_PLUGIN:?"Did not find grpc_tools_ruby_protoc_plugin"}"
 
 protoc \
-  --gofast_out=plugins=grpc:"${PERM_GO_PATH}" \
+  --gofast_out=plugins=grpc:"${PROTOS_PATH}/gen" \
   --ruby_out="${PERM_RB_PATH}/lib/perm/protos" \
   --plugin=protoc-gen-grpc="$RUBY_PROTOC_PLUGIN" \
   --grpc_out="${PERM_RB_PATH}/lib/perm/protos" \
-  -I="${PROTOS_PATH}:${PERM_GO_PATH}/vendor" \
+  -I="${PROTOS_PATH}:${PERM_PACKAGE_PATH}/vendor" \
   "${PROTOS_PATH}/"*.proto
