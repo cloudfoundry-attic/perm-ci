b4c7c3af4fcb1da70d770f57d6bc8185d2e0f1e1
Slawek Ligus <sligus@pivotal.io>
2018-04-26 16:09:29 -0700

    Push gems
    [#156770928]

--- tasks/publish-ruby-client/task.sh
+++ tasks/publish-ruby-client/task.sh
@@ -4,12 +4,19 @@ set -eu
 
 VERSION="$(cat version/version)"
 
+mkdir -p ${HOME}/.gem
+cat << EOF > ${HOME}/.gem/credentials
+---
+:rubygems_api_key: ${RUBYGEMS_API_KEY}
+EOF
+
 pushd perm-rb
   # Something's busted with the ruby docker image, so this is the workaround
   # https://github.com/bundler/bundler/issues/6162
   export BUNDLE_GEMFILE="${PWD}/Gemfile"
   bundle install
-  bundle exec rake
+  gem build *.gemspec
+  for GEM in *.gem; do
+    gem push $GEM
+  done
 popd
-
-exit 1
