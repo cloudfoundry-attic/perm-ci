2ddfef5cbb5b8c4627adb586485f77659db4dfc3
Christopher Brown <cbrown@pivotal.io>
2018-05-03 11:19:49 -0700

    Allow for globbing, remove irrelevant reference to a bug
    [#157269757]
    
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- tasks/test-ruby-client/task.sh
+++ tasks/test-ruby-client/task.sh
@@ -2,12 +2,9 @@
 
 set -eu
 
-tar -zxf "${PWD}/perm-bin-dir/perm-*.tgz" -C /usr/local/bin
+tar -zxf "${PWD}/perm-bin-dir/"perm-*.tgz -C /usr/local/bin
 
 pushd perm-rb
-  # Something's busted with the ruby docker image, so this is the workaround
-  # https://github.com/bundler/bundler/issues/6162
-  export BUNDLE_GEMFILE="${PWD}/Gemfile"
   bundle install
   bundle exec rake
 popd
