bdccd2683bab73b6ebd2c8e86e1ea9d4fac002bc
Christopher Brown <cbrown@pivotal.io>
2018-05-03 11:12:48 -0700

    Remove obsolete mysql setup from ruby client test
    [#157269757]
    
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- tasks/test-ruby-client/task.sh
+++ tasks/test-ruby-client/task.sh
@@ -4,17 +4,6 @@ set -eu
 
 tar -zxf "${PWD}/perm-bin-dir/perm-*.tgz" -C /usr/local/bin
 
-# From capi-ci:
-# HACK: change access time on mysql files to copy them into the writable layer
-# Context: https://github.com/moby/moby/issues/34390
-find /var/lib/mysql/mysql -exec touch -c -a {} +
-
-# We don't need to care about authentication here,
-# and for some reason the driver has trouble authenticating if we require authentication.
-# Ergo, no authentication in unit tests!
-mysqld_safe --skip-grant-tables > /dev/null &
-trap "killall -u mysql" EXIT
-
 pushd perm-rb
   # Something's busted with the ruby docker image, so this is the workaround
   # https://github.com/bundler/bundler/issues/6162
