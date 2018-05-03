126d10b888c7a6528af7f35afb43204f2a28ce61
Slawek Ligus <sligus@pivotal.io>
2018-05-03 11:22:15 -0700

    test-ruby-client: this is still required
    [#157269757]
    
    Signed-off-by: Christopher Brown <cbrown@pivotal.io>

--- tasks/test-ruby-client/task.sh
+++ tasks/test-ruby-client/task.sh
@@ -5,6 +5,9 @@ set -eu
 tar -zxf "${PWD}/perm-bin-dir/"perm-*.tgz -C /usr/local/bin
 
 pushd perm-rb
+  # Something's busted with the ruby docker image, so this is the workaround
+  # https://github.com/bundler/bundler/issues/6162
+  export BUNDLE_GEMFILE="${PWD}/Gemfile"
   bundle install
   bundle exec rake
 popd
