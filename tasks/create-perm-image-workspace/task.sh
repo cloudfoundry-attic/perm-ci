9b2a205e949db0d297f07e6bfd5004bd02fd1235
Jeff Pak <jeffrey.pak@emc.com>
2018-06-22 13:59:05 -0700

    move certs into their own directory
    [#158434830]

--- tasks/create-perm-image-workspace/task.sh
+++ tasks/create-perm-image-workspace/task.sh
@@ -6,4 +6,4 @@ tar -zxf ./perm-bin-dir/perm-*.tgz -C perm-image-workspace
 cp perm-image-src/images/perm/Dockerfile perm-image-workspace
 
 mkdir -p perm-image-workspace/certs
-cp perm-image-src/images/perm/certs/* perm-image-workspace/certs/
+cp perm-certs/certs/* perm-image-workspace/certs/
