5460fb468bf87722ffaa8bf63fe3610857ae8135
Nick Wei <nwei@pivotal.io>
2018-07-03 12:33:22 -0700

    adds gofmt, goimports, and go vet to our ci tests
    [finishes #157104615]

--- tasks/test-server/task.sh
+++ tasks/test-server/task.sh
@@ -12,6 +12,29 @@ mysqld_safe --skip-grant-tables > /dev/null &
 trap "killall -u mysql" EXIT
 
 cd "${GOPATH}/src/code.cloudfoundry.org/perm"
-go install code.cloudfoundry.org/perm/vendor/github.com/onsi/ginkgo/ginkgo
 
+go fmt ./...
+
+DIRECTORIES="./cmd ./integration ./pkg ./protos ./test"
+
+echo "########################"
+echo "# go vet output"
+echo "########################"
+set +e
+go tool vet -all -shadow $DIRECTORIES
+set -e
+echo "########################"
+echo "# end go vet output"
+echo "########################"
+
+echo "########################"
+echo "# goimports output"
+echo "########################"
+echo "The following files have import mismatches with goimports:"
+goimports -l $DIRECTORIES
+echo "########################"
+echo "# end goimports output"
+echo "########################"
+
+go install code.cloudfoundry.org/perm/vendor/github.com/onsi/ginkgo/ginkgo
 ginkgo -r -p -race -randomizeSuites -randomizeAllSpecs
