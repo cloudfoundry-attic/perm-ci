7ff1fc7e07b3361b916dcc96478f7948239b2606
Nick Wei <nwei@pivotal.io>
2018-07-16 17:24:34 -0700

    adds task to rebase against cf-deployment
    [#159090691]
    
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- tasks/rebase-cfd/task.sh
+++ tasks/rebase-cfd/task.sh
@@ -1,3 +1,13 @@
-#!/bin/bash -eu
+#!/bin/bash
 
-echo rebase
+set -eux
+
+git config --global user.email "cf-permissions@pivotal.io"
+git config --global user.name "CI Bot"
+
+cd cf-deployment-perm
+
+git remote add -f local-release-candidate ../cf-deployment-release-candidate
+git branch release-candidate-perm
+git rebase local-release-candidate/release-candidate
+cp -a . ../rebased-cf-deployment-perm
